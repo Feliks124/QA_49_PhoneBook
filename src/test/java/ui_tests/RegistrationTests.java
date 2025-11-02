0e0909cf96c4d7474c9e3bfeebf0e4ca9b0a2b81
Feliks124 <insuon124@gmail.com>
2025-11-02 20:02:34 +0200

    scrollValidator

--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -2,7 +2,6 @@ package ui_tests;
 
 import dto.User;
 import manager.ApplicationManager;
-import org.checkerframework.checker.units.qual.A;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,13 +18,13 @@ public class RegistrationTests extends ApplicationManager {
     LoginPage loginPage;
 
     @BeforeMethod
-    public void goToRegPage(){
+    public void goToRegPage() {
         new HomePage(getDriver()).clickBtnLoginHeader();
         loginPage = new LoginPage(getDriver());
     }
 
     @Test
-    public void registrationPositiveTest(){
+    public void registrationPositiveTest() {
         User user = randomUser(8);
         loginPage.typeRegForm(user);
         Assert.assertTrue(new ContactsPage(getDriver())
@@ -33,7 +32,7 @@ public class RegistrationTests extends ApplicationManager {
     }
 
     @Test
-    public void registrationNegativeTest_wrongEmail(){
+    public void registrationNegativeTest_wrongEmail() {
         User user = new User("wrong email", generatePassword(8));
         loginPage.typeRegForm(user);
         Assert.assertTrue(loginPage.closeAlertReturnText()
@@ -41,23 +40,23 @@ public class RegistrationTests extends ApplicationManager {
     }
 
     @Test
-    public void registrationNegativeTest_wrongPassword(){
-        User user = new User(randomValidEmail(),"wrong password");
+    public void registrationNegativeTest_wrongPassword() {
+        User user = new User(randomValidEmail(), "wrong password");
         loginPage.typeRegForm(user);
         Assert.assertTrue(loginPage.closeAlertReturnText()
                 .contains("Wrong email or password format"));
     }
 
     @Test
-    public void registrationNegativeTest_withoutData(){
-        User user = new User("","");
+    public void registrationNegativeTest_withoutData() {
+        User user = new User("", "");
         loginPage.typeRegForm(user);
         Assert.assertTrue(loginPage.closeAlertReturnText()
                 .contains("Wrong email or password format"));
     }
 
     @Test
-    public void registrationNegativeTest_DuplicateUser(){
+    public void registrationNegativeTest_DuplicateUser() {
         User user = randomUser(8);
         loginPage.typeRegForm(user);
         Assert.assertTrue(new ContactsPage(getDriver())
